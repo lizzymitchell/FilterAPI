36d8ff25257c589ebf070666cc4a49f90d36a900
Lizzy Mithell <hello@lizzymitchell.co.uk>
2023-05-01 10:09:23 +0100

    added unit tests for ListIsMatch

--- FilterAPI/src/test/java/FilterTest.java
+++ FilterAPI/src/test/java/FilterTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class FilterTest {
 
     @Test
-    void test_empty_query() {
+    void test_matches_empty_query() {
         Map<String, String> user = new LinkedHashMap<String, String>();
         user.put("firstname", "Joe");
 
@@ -22,7 +22,7 @@ class FilterTest {
     }
 
     @Test
-    void test_empty_user(){
+    void test_matches_empty_user(){
         Map<String, String> user = new LinkedHashMap<String, String>();
 
         List<QueryItem> queryItemList = new ArrayList<QueryItem>();
@@ -35,5 +35,119 @@ class FilterTest {
     }
 
 
+    @Test
+    void test_listIsMatch_empty_list_returns_false() {
+        List<QueryItem> list = new ArrayList<>();
+        Filter filter = new Filter();
 
+        boolean match = filter.ListIsMatch(list);
+        assertFalse(match);
+    }
+
+    @Test
+    void test_ListIsMatch_all_false_and_returns_false() {
+
+        QueryItem queryItem = new QueryItem();
+        queryItem.Matches = false;
+        queryItem.Operator = Operator.AND;
+
+        List<QueryItem> list = new ArrayList<>();
+        list.add(queryItem);
+        list.add(queryItem);
+
+        Filter filter = new Filter();
+        boolean match = filter.ListIsMatch(list);
+        assertFalse(match);
+    }
+
+    @Test
+    void test_ListIsMatch_all_false_or_returns_false() {
+        QueryItem queryItem = new QueryItem();
+        queryItem.Matches = false;
+        queryItem.Operator = Operator.OR;
+
+        List<QueryItem> list = new ArrayList<>();
+        list.add(queryItem);
+        list.add(queryItem);
+
+        Filter filter = new Filter();
+        boolean match = filter.ListIsMatch(list);
+        assertFalse(match);
+    }
+
+    @Test
+    void test_ListIsMatch_some_true_and_returns_false() {
+        QueryItem queryItem = new QueryItem();
+        queryItem.Matches = false;
+        queryItem.Operator = Operator.AND;
+        QueryItem trueQueryItem = new QueryItem();
+        trueQueryItem.Matches = true;
+        trueQueryItem.Operator = Operator.AND;
+
+        List<QueryItem> list = new ArrayList<>();
+        list.add(queryItem);
+        list.add(queryItem);
+        list.add(trueQueryItem);
+
+        Filter filter = new Filter();
+        boolean match = filter.ListIsMatch(list);
+        assertFalse(match);
+    }
+
+    @Test
+    void test_ListIsMatch_one_true_or_returns_true() {
+        QueryItem queryItem = new QueryItem();
+        queryItem.Matches = false;
+        queryItem.Operator = Operator.AND;
+        QueryItem trueQueryItem = new QueryItem();
+        trueQueryItem.Matches = true;
+        trueQueryItem.Operator = Operator.OR;
+
+        List<QueryItem> list = new ArrayList<>();
+        list.add(queryItem);
+        list.add(queryItem);
+        list.add(trueQueryItem);
+
+        Filter filter = new Filter();
+        boolean match = filter.ListIsMatch(list);
+        assertTrue(match);
+    }
+
+    @Test
+    void test_ListIsMatch_all_true_and_returns_true(){
+        QueryItem queryItem = new QueryItem();
+        queryItem.Matches = true;
+        queryItem.Operator = Operator.AND;
+
+        List<QueryItem> list = new ArrayList<>();
+        list.add(queryItem);
+        list.add(queryItem);
+
+        Filter filter = new Filter();
+        boolean match = filter.ListIsMatch(list);
+        assertTrue(match);
+    }
+
+    @Test
+    void test_ListIsMatch_all_true_or_returns_true(){
+        QueryItem queryItem = new QueryItem();
+        queryItem.Matches = true;
+        queryItem.Operator = Operator.OR;
+
+        List<QueryItem> list = new ArrayList<>();
+        list.add(queryItem);
+        list.add(queryItem);
+
+        Filter filter = new Filter();
+        boolean match = filter.ListIsMatch(list);
+        assertTrue(match);
+    }
+
+    @Test
+    void matches() {
+    }
+
+    @Test
+    void testMatches() {
+    }
 }
